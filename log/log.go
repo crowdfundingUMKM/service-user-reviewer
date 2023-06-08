6a0e80b1f5ab8290f2c10d9a06b66857c4797b45
ahhzaky <siahmadzaky@gmail.com>
2023-06-08 17:51:00 +0700

    setup log service but not use

--- log/log.go
+++ log/log.go
@@ -1,1 +1,17 @@
 package log
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitLog() {
+	f, err := os.Create("./log/gin.log")
+	if err != nil {
+		log.Fatal("cannot create open gin.log", err)
+	}
+	gin.DefaultWriter = io.MultiWriter(f)
+}
